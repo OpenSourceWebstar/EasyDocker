5ef3812b13bb06c4ba7ab5c206f1be64b98271ba
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-09-01 19:18:17 +0100

    * Updated migration with fixed variables.

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+migrateScanFoldersForUpdates()
+{
+    # Loop through all directories in the install path
+    for folder in "$install_path"/*; do
+        if [ -d "$folder" ]; then
+            migrateCheckAndUpdateIP $folder;
+            migrateCheckAndUpdateInstallName $folder;
+        fi
+    done
+
+    isSuccessful "Migration IP checking and updating completed."
+}
+
 migrateGenerateTXTAll()
 {
     # Loop through subdirectories
-    for dir in "$install_path"/*; do
-        if [ -d "$dir" ]; then
+    for folder in "$install_path"/*; do
+        if [ -d "$folder" ]; then
             # Check if a migrate.txt file exists in the current directory
             if [ ! -f "$migrate_file" ]; then
-                migrateBuildTXT $dir;
+                migrateBuildTXT $folder;
             fi
         fi
     done
@@ -17,12 +30,13 @@ migrateGenerateTXTAll()
 
 migrateGenerateTXTSingle()
 {
-    app_name=$1
+    local folder=$1
+    local app_name=$(basename "$folder")
     # Check if the specified directory exists
-    if [ -d "$install_path/$app_name" ]; then
+    if [ -d "$folder" ]; then
         # Check if a migrate.txt file already exists in the specified directory
-        if [ ! -f "$install_path/$app_name/$migrate_file" ]; then
-            migrateBuildTXT $app_name;
+        if [ ! -f "$folder/$migrate_file" ]; then
+            migrateBuildTXT $folder;
         else
             isNotice "$migrate_file already exists for $app_name."
             while true; do
@@ -34,7 +48,7 @@ migrateGenerateTXTSingle()
                 isNotice "Please provide a valid input (y/n)."
             done
             if [[ "$replacemigration" == [yY] ]]; then
-                migrateBuildTXT $app_name;
+                migrateBuildTXT $folder;
             fi
         fi
     else
@@ -46,9 +60,10 @@ migrateGenerateTXTSingle()
 
 migrateBuildTXT()
 {
-    local app_name=$1
+    local folder=$1
+    local app_name=$(basename "$folder")
     # Create a migrate.txt file with IP and InstallName
-    result=$(touch "$install_path/$app_name/$migrate_file")
+    result=$(touch "$folder/$migrate_file")
     checkSuccess "Generated $migrate_file for $app_name"
     result=$(echo "MIGRATE_IP=$public_ip" > "$install_path/$app_name/$migrate_file")
     checkSuccess "Adding MIGRATE_IP $public_ip to $migrate_file"
@@ -60,19 +75,19 @@ migrateBuildTXT()
 
 migrateCheckAndUpdateIP()
 {
-    local app_name="$1"
-
+    local folder="$1"
+    local app_name=$(basename "$folder")
     # Check if the migrate.txt file exists
     if [ -f "$migrate_file" ]; then
-        local migrate_ip=$(grep -o 'MIGRATE_IP=.*' "$install_path/$app_name/$migrate_file" | cut -d '=' -f 2)
+        local migrate_ip=$(grep -o 'MIGRATE_IP=.*' "$folder/$migrate_file" | cut -d '=' -f 2)
         if [ "$migrate_ip" = "$public_ip" ]; then
             isSuccessful "MIGRATE_IP is already set correctly in $migrate_file."
         else
-            result=$(sed -i "s/MIGRATE_IP=.*/MIGRATE_IP=$public_ip/" "$install_path/$app_name/$migrate_file")
+            result=$(sed -i "s/MIGRATE_IP=.*/MIGRATE_IP=$public_ip/" "$folder/$migrate_file")
             checkSuccess "Updated MIGRATE_IP in $migrate_file to $public_ip."
 
             # Replace old IP with $public_ip in .yml and .env files
-            result=$(find "$full_path" -type f \( -name "*.yml" -o -name "*.env" \) -exec sed -i "s/$migrate_ip/$public_ip/g" {} \;)
+            result=$(find "$folder" -type f \( -name "*.yml" -o -name "*.env" \) -exec sed -i "s/$migrate_ip/$public_ip/g" {} \;)
             checkSuccess "Replaced old IP with $public_ip in .yml and .env files in $app_name."
         fi
     else
@@ -82,31 +97,18 @@ migrateCheckAndUpdateIP()
 
 migrateCheckAndUpdateInstallName() 
 {
-    local app_name="$1"
-
+    local folder="$1"
+    local app_name=$(basename "$folder")
     # Check if the migrate.txt file exists
     if [ -f "$migrate_file" ]; then
-        local migrate_install_name=$(grep -o 'MIGRATE_INSTALL_NAME=.*' "$install_path/$app_name/$migrate_file" | cut -d '=' -f 2)
+        local migrate_install_name=$(grep -o 'MIGRATE_INSTALL_NAME=.*' "$folder/$migrate_file" | cut -d '=' -f 2)
         if [ "$migrate_install_name" = "$CFG_INSTALL_NAME" ]; then
             isSuccessful "MIGRATE_INSTALL_NAME is already set correctly in $migrate_file."
         else
-            result=$(sed -i "s/MIGRATE_INSTALL_NAME=.*/MIGRATE_INSTALL_NAME=$CFG_INSTALL_NAME/" "$install_path/$app_name/$migrate_file")
+            result=$(sed -i "s/MIGRATE_INSTALL_NAME=.*/MIGRATE_INSTALL_NAME=$CFG_INSTALL_NAME/" "$folder/$migrate_file")
             checkSuccess "Updated MIGRATE_INSTALL_NAME in $migrate_file to $CFG_INSTALL_NAME."
         fi
     else
         isError "$migrate_file not found in $app_name."
     fi
-}
-
-migrateScanFoldersForUpdates()
-{
-    # Loop through all directories in the install path
-    for folder in "$install_path"/*; do
-        if [ -d "$folder" ]; then
-            migrateCheckAndUpdateIP "$folder"
-            migrateCheckAndUpdateInstallName "$folder"
-        fi
-    done
-
-    isSuccessful "Migration IP checking and updating completed."
 }
