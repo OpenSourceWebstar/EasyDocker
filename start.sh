db5a5ddeb0fb9a847167d65845e7d6a3f60a73a6
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-09-08 23:20:15 +0100

    * Added Quad9 IP to internet check.

--- start.sh
+++ start.sh
@@ -19,7 +19,7 @@ checkUpdates()
 
 		# Internet Test
 		isNotice "Testing internet, please wait..."
-		if sudo ping -c 4 "$ip_address" > /dev/null; then
+		if sudo ping -c 4 "9.9.9.9" > /dev/null; then
 			isSuccess "Internet connectivity is working."
 		else
 			isError "Internet connectivity is not working."
