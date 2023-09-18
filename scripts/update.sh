866344dbc6bd138c376e79f45a5fe5258dece3b7
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-09-18 03:04:26 +0100

    * Removed unneeded logic from gitReset.

--- scripts/update.sh
+++ scripts/update.sh
@@ -214,7 +214,7 @@ gitUntrackFiles()
     sudo -u $easydockeruser git rm --cached $configs_dir/$ip_file > /dev/null 2>&1
     sudo -u $easydockeruser git rm --cached $logs_dir/$docker_log_file > /dev/null 2>&1
     sudo -u $easydockeruser git rm --cached $logs_dir/$backup_log_file > /dev/null 2>&1
-    #sudo -u $easydockeruser git clean -f "*.config" > /dev/null 2>&1
+    sudo -u $easydockeruser git clean -f "*.config" > /dev/null 2>&1
     isSuccessful "Removing configs and logs from git for git changes"
     result=$(sudo -u $easydockeruser git commit -m "Stop tracking ignored files")
     checkSuccess "Removing tracking ignored files"
@@ -222,7 +222,6 @@ gitUntrackFiles()
 
 gitReset()
 {
-    if [[ $customupdatesfound == [nN] ]]; then
         # Reset git
         result=$(sudo -u $easydockeruser rm -rf $script_dir)
         checkSuccess "Deleting all Git files"
@@ -232,7 +231,6 @@ gitReset()
         checkSuccess "Going into the install folder"
         result=$(sudo -u $easydockeruser git clone "$repo_url" "$script_dir" > /dev/null 2>&1)
         checkSuccess "Clone the Git repository"
-    fi
 }
 
 gitCleanInstallBackups()
