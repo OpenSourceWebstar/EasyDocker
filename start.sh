942b15e3eae0329d99b75ac27b559fada50d7058
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-09-22 22:07:24 +0100

    * New approach to the fixFolderPermissions.

--- start.sh
+++ start.sh
@@ -63,6 +63,7 @@ startPreInstall()
 startScan()
 {
 	databasePathInsert $initial_path_save
+	fixFolderPermissions;
 	if [[ $CFG_REQUIREMENT_MIGRATE == "true" ]]; then
 		migrateCheckForMigrateFiles;
 		migrateGenerateTXTAll;
