ed58f102390b165c3d4a96ce8be6dcb4d989e028
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-09-24 12:57:11 +0100

    * Unedited default values for whitelist are now updated.

--- scripts/whitelist.sh
+++ scripts/whitelist.sh
@@ -54,6 +54,16 @@ whitelistUpdateYML()
         if [ -f "$yaml_file" ]; then
             # Check if the YAML file contains ipwhitelist.sourcerange
             if grep -q "ipwhitelist.sourcerange:" "$yaml_file"; then
+
+                # Whitelist not setup yet
+                if grep -q "ipwhitelist.sourcerange: IPWHITELIST" "$yaml_file"; then
+                    echo "whitelistUpdateCompose $app_name $app_config;"
+                    whitelistUpdateCompose $app_name $app_config;
+                    echo "whitelistUpdateRestart $app_name $app_script $should_restart;"
+                    whitelistUpdateRestart $app_name $app_script $should_restart;
+                fi
+                
+                # If the IPs are setup already but needs an update
                 local current_ip_range=$(grep "ipwhitelist.sourcerange:" "$yaml_file" | cut -d ' ' -f 2)
                 if [ "$current_ip_range" != "$CFG_IPS_WHITELIST" ]; then
                     result=$(sudo sed -i "s/ipwhitelist.sourcerange: $current_ip_range/ipwhitelist.sourcerange: $CFG_IPS_WHITELIST/" "$yaml_file")
@@ -68,6 +78,18 @@ whitelistUpdateYML()
     done
     if [[ "$app_name" == "fail2ban" ]]; then
         if grep -q "ignoreip = ips_whitelist" "$install_dir/$app_name/config/$app_name/jail.local"; then
+
+            # Whitelist not setup yet
+            if grep -q "ignoreip = ips_whitelist" "$yaml_file"; then
+                result=$(sudo sed -i "s/ips_whitelist/$CFG_IPS_WHITELIST/" "$install_dir/$app_name/config/$app_name/jail.local")
+                checkSuccess "Update the IP whitelist for $app_name"
+                echo "whitelistUpdateCompose $app_name $app_config;"
+                whitelistUpdateCompose $app_name $app_config;
+                echo "whitelistUpdateRestart $app_name $app_script $should_restart;"
+                whitelistUpdateRestart $app_name $app_script $should_restart;
+            fi
+
+            # If the IPs are setup already but needs an update
             local current_ip_range=$(grep "ignoreip = " "$install_dir/$app_name/config/$app_name/jail.local" | cut -d ' ' -f 2)
             if [ "$current_ip_range" != "$CFG_IPS_WHITELIST" ]; then
                 result=$(sudo sed -i "s/ignoreip = ips_whitelist/ignoreip = $CFG_IPS_WHITELIST/" "$install_dir/$app_name/config/$app_name/jail.local")
