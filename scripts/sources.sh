5cc0967b91dd9a00fcab192b2659f8763abaa110
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-10-04 00:35:13 +0100

    * Fixed issue with reinstallation not exiting EasyDocker.

--- scripts/sources.sh
+++ scripts/sources.sh
@@ -106,12 +106,13 @@ sourceScript()
 
 runInitReinstall() 
 {
-	echo ""
-	echo "####################################################"
-	echo "###           Reinstalling EasyDocker            ###"
-	echo "####################################################"
-	echo ""
-    sudo bash -c 'cd ~ && rm -rf init.sh && apt-get install wget -y && wget -O init.sh https://raw.githubusercontent.com/OpenSourceWebstar/EasyDocker/main/init.sh && chmod 0755 init.sh && ./init.sh run && exit'
+    echo ""
+    echo "####################################################"
+    echo "###           Reinstalling EasyDocker            ###"
+    echo "####################################################"
+    echo ""
+    sudo bash -c 'cd ~ && rm -rf init.sh && apt-get install wget -y && wget -O init.sh https://raw.githubusercontent.com/OpenSourceWebstar/EasyDocker/main/init.sh && chmod 0755 init.sh && ./init.sh run'
+    exit 0  # Exit the entire script
 }
 
 sourceScript;
