a6194d67284c28c8b74a841e4472aeebcb00cfbd
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-10-05 23:49:49 +0100

    * Changed the way the silent flag works in setupConfigToContainer.

--- containers/mailcow/mailcow.sh
+++ containers/mailcow/mailcow.sh
@@ -51,7 +51,7 @@ installMailcow()
         echo "---- $menu_number. Setting up install folder and config file for $app_name."
         echo ""
 
-        setupConfigToContainer $app_name install;
+        setupConfigToContainer "" $app_name install;
         isSuccessful "Install folders and Config files have been setup for $app_name."
 
 		((menu_number++))
