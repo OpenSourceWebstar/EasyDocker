7ad42b1b71fd48fa413169b0bca3384e054032f0
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-10-13 22:58:52 +0100

    * Reworked the way the edit config file on install works.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -68,57 +68,76 @@ setupConfigToContainer()
     else
         if [[ "$flags" == "install" ]]; then
             if [ -f "$target_path/$app_name.config" ]; then
-                echo ""
-                isNotice "Config file for $app_name has been found..."
-                echo ""
-                while true; do
-                    isQuestion "Would you like to reset to defaults (r) or edit (e) the config file? (r/e): "
-                    read -rp "" resetconfigaccept
+                # Same content check
+                if cmp -s "$source_file" "$target_path/$app_name.config"; then
                     echo ""
-                    case $resetconfigaccept in
-                        [eE])
-                             # Calculate the checksum of the original file
-                            local original_checksum=$(md5sum "$target_path/$app_name.config")
+                    isNotice "Config file for $app_name contains no edits."
+                    echo ""
+                    while true; do
+                        isQuestion "Would you like to make edits to the config file? (y/n): "
+                        read -rp "" editconfigaccept
+                        echo ""
+                        case $editconfigaccept in
+                            [yY])
+                                # Calculate the checksum of the original file
+                                local original_checksum=$(md5sum "$target_path/$app_name.config")
 
-                            # Open the file with nano for editing
-                            sudo nano "$target_path/$app_name.config"
+                                # Open the file with nano for editing
+                                sudo nano "$target_path/$app_name.config"
 
-                            # Calculate the checksum of the edited file
-                            local edited_checksum=$(md5sum "$target_path/$app_name.config")
+                                # Calculate the checksum of the edited file
+                                local edited_checksum=$(md5sum "$target_path/$app_name.config")
 
-                            # Compare the checksums to check if changes were made
-                            if [[ "$original_checksum" != "$edited_checksum" ]]; then
-                                isSuccessful "Changes have been made to the $app_name.config."
-                            fi
-                            break
-                            ;;
-                        [rR])
-                            isNotice "Resetting $app_name config file."
-                            copyFile "$source_file" "$target_path/$app_name.config" | sudo -u $easydockeruser tee -a "$logs_dir/$docker_log_file" 2>&1
-                            break
-                            ;;
-                        *)
-                            isNotice "Please provide a valid input (y or n)."
-                            ;;
-                    esac
-                done
-            else
-                if [ -z "$silent_flag" ]; then
-                    isNotice "Config file for $app_name does not exist. Creating it..."
-                    copyFile "$source_file" "$target_path/$app_name.config" | sudo -u $easydockeruser tee -a "$logs_dir/$docker_log_file" 2>&1
+                                # Compare the checksums to check if changes were made
+                                if [[ "$original_checksum" != "$edited_checksum" ]]; then
+                                    isSuccessful "Changes have been made to the $app_name.config."
+                                fi
+                                break
+                                ;;
+                            [nN])
+                                break  # Exit the loop without updating
+                                ;;
+                            *)
+                                isNotice "Please provide a valid input (y or n)."
+                                ;;
+                        esac
+                    done
                 else
-                    copyFile "$silent_flag" "$source_file" "$target_path/$app_name.config" | sudo -u $easydockeruser tee -a "$logs_dir/$docker_log_file" 2>&1
+                    echo ""
+                    isNotice "Config file for $app_name has been updated..."
+                    echo ""
+                    while true; do
+                        isQuestion "Would you like to reset the config file? (y/n): "
+                        read -rp "" resetconfigaccept
+                        echo ""
+                        case $resetconfigaccept in
+                            [yY])
+                                isNotice "Resetting $app_name config file."
+                                copyFile "$source_file" "$target_path/$app_name.config" | sudo -u $easydockeruser tee -a "$logs_dir/$docker_log_file" 2>&1
+                                break  # Exit the loop after executing whitelistAndStartApp
+                                ;;
+                            [nN])
+                                break  # Exit the loop without updating
+                                ;;
+                            *)
+                                isNotice "Please provide a valid input (y or n)."
+                                ;;
+                        esac
+                    done
                 fi
+            else
+                isNotice "Config file for $app_name does not exist. Creating it..."
+                copyFile "$source_file" "$target_path/$app_name.config" | sudo -u $easydockeruser tee -a "$logs_dir/$docker_log_file" 2>&1
                 echo ""
-                isNotice "Config file for $app_name has been found..."
+                isNotice "Config file for $app_name contains no edits."
                 echo ""
                 while true; do
-                    isQuestion "Would you like to reset to defaults (r) or edit (e) the config file? (r/e):"
-                    read -rp "" resetconfigaccept
+                    isQuestion "Would you like to make edits to the config file? (y/n): "
+                    read -rp "" editconfigaccept
                     echo ""
-                    case $resetconfigaccept in
-                        [eE])
-                             # Calculate the checksum of the original file
+                    case $editconfigaccept in
+                        [yY])
+                            # Calculate the checksum of the original file
                             local original_checksum=$(md5sum "$target_path/$app_name.config")
 
                             # Open the file with nano for editing
@@ -133,10 +152,8 @@ setupConfigToContainer()
                             fi
                             break
                             ;;
-                        [rR])
-                            isNotice "Resetting $app_name config file."
-                            copyFile "$source_file" "$target_path/$app_name.config" | sudo -u $easydockeruser tee -a "$logs_dir/$docker_log_file" 2>&1
-                            break
+                        [nN])
+                            break  # Exit the loop without updating
                             ;;
                         *)
                             isNotice "Please provide a valid input (y or n)."
