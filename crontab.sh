109e1493471f184a01cbf7f0c495e24ff304b18f
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-10-19 00:32:21 +0100

    * Fixed backups not working and loading EasyDocker instead.

--- crontab.sh
+++ crontab.sh
@@ -3,7 +3,35 @@
 app_name="$1"
 param2="$2"
 
-source scripts/sources.sh
+source init.sh
+source scripts/backup.sh
+source scripts/functions.sh
+source scripts/docker.sh
+source scripts/database.sh
+source scripts/permissions.sh
+source scripts/variables.sh
+source scripts/network.sh
+
+loadFiles() 
+{
+    local load_type="$1"
+    local file_pattern
+
+    if [ "$load_type" = "easydocker_configs" ]; then
+        local file_pattern="config_*"
+        local folder_dir="$configs_dir"
+    else
+        echo "Invalid load type: $load_type"
+        return
+    fi
+
+    while IFS= read -r -d '' file; do
+        if [ -f "$file" ]; then
+            source "$(echo "$file" | sed 's|/docker/install//||')"
+            #echo "$load_type FILE $(echo "$file" | sed 's|/docker/install//||')"
+        fi
+    done < <(sudo find "$folder_dir" -type d \( -name 'resources' \) -prune -o -type f -name "$file_pattern" -print0)
+}
 
 #Used for the backup script to call the function
 crontabInitilize()
@@ -22,4 +50,5 @@ crontabInitilize()
 
 }
 
-crontabInitilize
+loadFiles "easydocker_configs";
+crontabInitilize;
