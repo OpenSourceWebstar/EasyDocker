02891e0b528873f9270f64220c0ef2b3f6b4cb33
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-10-28 06:10:11 +0100

    * Fixed issued with it not sourcing files correctly.

--- scripts/sources.sh
+++ scripts/sources.sh
@@ -122,17 +122,16 @@ loadFiles()
 {
     local load_type="$1"
     local file_pattern
-    local folder_dir
 
     if [ "$load_type" = "easydocker_configs" ]; then
-        file_pattern="config_*"
-        folder_dir="$configs_dir"
+        local file_pattern="config_*"
+        local folder_dir="$configs_dir"
     elif [ "$load_type" = "app_configs" ]; then
-        file_pattern="*.config"
-        folder_dir="$containers_dir"
+        local file_pattern="*.config"
+        local folder_dir="$containers_dir"
     elif [ "$load_type" = "containers" ]; then
-        file_pattern="*.sh"
-        folder_dir="$install_containers_dir"
+        local file_pattern="*.sh"
+        local folder_dir="$install_containers_dir"
     else
         echo "Invalid load type: $load_type"
         return
@@ -141,10 +140,9 @@ loadFiles()
     while IFS= read -r -d '' file; do
         if [ -f "$file" ]; then
             source "$(echo "$file" | sed 's|/docker/install//||')"
-            # echo "$load_type FILE $(echo "$file" | sed 's|/docker/install//||')"
+            #echo "$load_type FILE $(echo "$file" | sed 's|/docker/install//||')"
         fi
-    done < <(sudo find "$folder_dir" -maxdepth 1 -type f -name "$file_pattern" -print0)
+    done < <(sudo find "$folder_dir" -maxdepth 1 -type d \( -name 'resources' \) -prune -o -type f -name "$file_pattern" -print0)
 }
-
 
 sourceScripts "start";
