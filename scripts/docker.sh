aad9e8655607bbdd1395821927e02dffb9ab8782
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-11-02 03:05:43 +0000

    * Fixed variable generaion for insallApp function.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -273,14 +273,14 @@ checkAppInstalled()
     echo "$package_status"
 }
 
-installApp()
-{
+installApp() {
     local app_name="$1"
     local app_name_ucfirst="$(tr '[:lower:]' '[:upper:]' <<< ${app_name:0:1})${app_name:1}"
     local installFuncName="install${app_name_ucfirst}"
 
-    # Create a variable with the name of $app_name and set its value to "i"
-    declare "$app_name=i"
+    # Create an associative array with the name of $app_name and set its value to "i"
+    declare -A app_data
+    app_data["$app_name"]="i"
 
     # Call the installation function
     ${installFuncName}
