62ea94d6afdb29ef3af3b5ce636f95835820d6cd
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-11-08 19:45:00 +0000

    * Added live logs to the log viewer.

--- scripts/logs.sh
+++ scripts/logs.sh
@@ -3,44 +3,53 @@
 viewLogsAppMenu()
 {
     local app_name="$1"
+
+    check app_name
     echo ""
     isNotice "Viewing logs for $app_name:"
     echo ""
-    isOption "1. Show last 20 lines"
-    isOption "2. Show last 50 lines"
-    isOption "3. Show last 100 lines"
-    isOption "4. Show last 200 lines"
-    isOption "5. Show full log"
+    isOption "1. Show the logs live"
+    isOption "2. Show last 20 lines"
+    isOption "3. Show last 50 lines"
+    isOption "4. Show last 100 lines"
+    isOption "5. Show last 200 lines"
+    isOption "6. Show full log"
     isOption "x. Back to main menu"
     echo ""
     isQuestion "Enter your choice (1-5, or 'x' to exit): "
     read -p "" app_log_choice
     case "$app_log_choice" in
         1)
-            runCommandForDockerInstallUser "docker logs $app_name --tail 20"
+            runCommandForDockerInstallUser "docker logs $app_name --follow"
             isQuestion "Press Enter to continue..."
             read -p "" continueafterlogs
             viewLogsAppMenu "$app_name"
         ;;
         2)
-            runCommandForDockerInstallUser "docker logs $app_name --tail 50"
+            runCommandForDockerInstallUser "docker logs $app_name --tail 20"
             isQuestion "Press Enter to continue..."
             read -p "" continueafterlogs
             viewLogsAppMenu "$app_name"
         ;;
         3)
-            runCommandForDockerInstallUser "docker logs $app_name --tail 100"
+            runCommandForDockerInstallUser "docker logs $app_name --tail 50"
             isQuestion "Press Enter to continue..."
             read -p "" continueafterlogs
             viewLogsAppMenu "$app_name"
         ;;
         4)
-            runCommandForDockerInstallUser "docker logs $app_name --tail 200"
+            runCommandForDockerInstallUser "docker logs $app_name --tail 100"
             isQuestion "Press Enter to continue..."
             read -p "" continueafterlogs
             viewLogsAppMenu "$app_name"
         ;;
         5)
+            runCommandForDockerInstallUser "docker logs $app_name --tail 200"
+            isQuestion "Press Enter to continue..."
+            read -p "" continueafterlogs
+            viewLogsAppMenu "$app_name"
+        ;;
+        6)
             runCommandForDockerInstallUser "docker logs $app_name"
             isQuestion "Press Enter to continue..."
             read -p "" continueafterlogs
