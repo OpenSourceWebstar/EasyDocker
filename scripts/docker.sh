31953720e688d4f472c5aab3ec68af10a067d691
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-11-21 01:41:46 +0000

    * Fixed permission issue with Whilelist builder.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -69,6 +69,12 @@ setupConfigToContainer()
 
     fixConfigPermissions $silent_flag $app_name;
 
+    # Check if the file exists
+    if [ ! -e "$target_path/$config_file" ]; then
+        isError "File $target_path/$config_file does not exist"
+        return
+    fi
+
     # Check if the user has read permission on target_path/config_file
     if [ ! -r "$target_path/$config_file" ]; then
         isError "Insufficient permissions to read $target_path/$config_file"
@@ -466,26 +472,26 @@ dockerScan()
     isSuccessful "All application whitelists are up to date."
 }
 
-dockerUpdateTraefikWhitelist()
+dockerUpdateTraefikWhitelist() 
 {
     local whitelist_file="${containers_dir}traefik/etc/dynamic/whitelist.yml"
     if [ -f "$whitelist_file" ]; then
         # Split the CFG_IPS_WHITELIST into an array
         IFS=',' read -ra IP_ARRAY <<< "$CFG_IPS_WHITELIST"
 
-# Build the YAML content dynamically
-YAML_CONTENT="http:
+        # Build the YAML content dynamically
+        YAML_CONTENT="http:
   middlewares:
     global-ipwhitelist:
       ipWhiteList:
         sourceRange:"
 
         for IP in "${IP_ARRAY[@]}"; do
-        YAML_CONTENT+="\n          - \"$IP\""
+            YAML_CONTENT+="\n          - \"$IP\""
         done
 
-        # Now update the YAML file with the new content
-        echo -e "$YAML_CONTENT" > $whitelist_file
+        # Now update the YAML file with the new content using sudo
+        echo -e "$YAML_CONTENT" | sudo tee "$whitelist_file" > /dev/null
         isSuccessful "Traefik has been updated with the latest whitelist IPs."
     fi
 }
