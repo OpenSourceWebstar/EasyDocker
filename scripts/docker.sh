2a8b3773942782af73d8cce120be381ca94dcc43
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-11-29 19:13:52 +0000

    * Fixed variable for installApp function.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -291,12 +291,12 @@ checkAppInstalled()
 
 installApp()
 {
-    local $app_name="$1"
+    local app_name="$1"
     local app_name_ucfirst="$(tr '[:lower:]' '[:upper:]' <<< ${app_name:0:1})${app_name:1}"
     local installFuncName="install${app_name_ucfirst}"
 
     # Create a variable with the name of $app_name and set its value to "i"
-    declare "$app_name=i"
+    declare "${app_name}=i"
 
     # Call the installation function
     ${installFuncName}
