c7b079536a3eaba92e22af1ee25a4a7007bb7b19
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-11-29 22:54:28 +0000

    * Added security check for the sshdownload status.

--- start.sh
+++ start.sh
@@ -21,6 +21,26 @@ startUp()
     if [[ "$traefik_status" == "installed" ]]; then
         traefikSetupLoginCredentials;
     fi
+	local sshdownload_status=$(checkAppInstalled "sshdownload" "docker")
+    if [[ "$sshdownload_status" == "installed" ]]; then
+        while true; do
+            echo ""
+            isNotice "The SSH Download download service is currently online."
+            isNotice "This is potentially DANGEROUS as it's accessable via anyone on the VPN"
+            isNotice "We highly recommend uninstalling this service after downloading the SSH keys"
+            isNotice "If you need to access this again, you can install it via the system install option"
+            echo ""
+            isQuestion "Would like to destroy the SSH Download service for security purposes? (y/n): "
+            read -p "" ssh_download_uninstall
+            if [[ -n "$ssh_download_uninstall" ]]; then
+                break
+            fi
+            isNotice "Please provide a valid input."
+        done
+        if [[ "$ssh_download_uninstall" == [yY] ]]; then
+            uninstallApp sshdownload;
+        fi
+    fi
     checkRequirements;
 }
 
