0c310e88c19313d4a1a2256520e3d4d448d0a5d7
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-12-01 21:59:27 +0000

    * Added setupSSHAuthorizedKeys functionality for SSH keys to work on the host.

--- scripts/install/install_ssh.sh
+++ scripts/install/install_ssh.sh
@@ -443,7 +443,43 @@ generateSSHKeyPair()
     result=$(echo "$ssh_passphrase" | sudo tee -a "$ssh_dir/public/$(basename $private_key_full)" > /dev/null)
     checkSuccess "Adding the ssh_passphrase to the $(basename "$private_key_full").txt file."
 
+    setupSSHAuthorizedKeys $username $public_key_full;
+
     ssh_new_key=true
+}
+
+setupSSHAuthorizedKeys()
+{
+    local username="$1"
+    local public_key_full="$2"
+
+    if [[ "$username" == "root" ]]; then
+        local ssh_path="/root/.ssh" 
+    else
+        local ssh_path="/home/$username/.ssh" 
+    fi
+
+    # Check if the directory exists; if not, create it
+    if [ ! -d "$ssh_path" ]; then
+        local result=$(mkdirFolders "loud" $username $ssh_path)
+        checkSuccess "Creating $(basename "$private_key_path") folder"
+        result=$(sudo chmod 700 $ssh_path)
+        checkSuccess "Updating permissions for $ssh_path"
+    else
+        result=$(sudo chmod 700 $ssh_path)
+        checkSuccess "Updating permissions for $ssh_path"
+    fi
+
+    if [ ! -f "$ssh_path/authorized_keys" ]; then
+        result=$(createTouch "$ssh_path/authorized_keys" $username)
+        checkSuccess "Creating the authorized_keys file for $username."
+    fi
+
+    result=$(sudo chmod 600 ${ssh_path}/authorized_keys)
+    checkSuccess "Updating permissions for ${username}'s authorized_keys file."
+
+    result=$(sudo cat ${ssh_path}/$(basename $public_key_full) >> ${ssh_path}/authorized_keys)
+    checkSuccess "Adding $(basename $public_key_full) to the Authorized_keys file for user $username"
 }
 
 disableSSHPasswords()
