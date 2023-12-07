4c5bb8bb667b10d46752704f9d4f0a0abd59c4de
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-12-07 00:40:10 +0000

    * Fixed tabbing for eof query for Invidious password updater.

--- scripts/applications.sh
+++ scripts/applications.sh
@@ -280,11 +280,11 @@ invidiousResetUserPassword()
             break
         fi
         if [[ "$invidiousresetconfirm" != [xX] ]]; then
-            runCommandForDockerInstallUser "docker exec invidious-invidious-db-1 /bin/bash -c '
-                su - postgres &&
-                psql -U kemal -d invidious -c "UPDATE users SET password = '\$2b\$10\$.ifJ1ph40pbjQqCLpstsgeHc5S7MbG.iBQ0nlwF8upMDjoBdFD35e' WHERE email = '$invidiousresetconfirm';" &&
-                exit
-            ' </dev/null"
+runCommandForDockerInstallUser "docker exec invidious-invidious-db-1 /bin/bash <<EOF
+    su - postgres &&
+    psql -U kemal -d invidious -c \"UPDATE users SET password = '\$2b\$10\$.ifJ1ph40pbjQqCLpstsgeHc5S7MbG.iBQ0nlwF8upMDjoBdFD35e' WHERE email = '$invidiousresetconfirm';\" &&
+    exit
+EOF"
             isSuccessful "If the user $invidiousresetconfirm exists, the new password will be 'password'"
             sleep 5;
             break
