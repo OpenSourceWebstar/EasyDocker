f2705d870ed1bdb36356694f339a665c5c422f5d
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-12-09 00:25:43 +0000

    * Fix logic for password disabled variable for SSH Keys.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -19,17 +19,17 @@ runCommandForDockerInstallUser()
 
     # Run the SSH command using the existing SSH variables
     local output
-    if [ "$passwordAuth" == "yes" ]; then
-        if [ -z "$silent_flag" ]; then
-            sshpass -p "$CFG_DOCKER_INSTALL_PASS" ssh -o StrictHostKeyChecking=no "$CFG_DOCKER_INSTALL_USER@localhost" "$remote_command"
-        else
-            sshpass -p "$CFG_DOCKER_INSTALL_PASS" ssh -o StrictHostKeyChecking=no "$CFG_DOCKER_INSTALL_USER@localhost" "$remote_command" > /dev/null 2>&1
-        fi
-    else
+    if [ "$passwordAuth" == "no" ]; then
         if [ -z "$silent_flag" ]; then
             ssh -i "${private_path}${install_user_key}" -o StrictHostKeyChecking=no "$CFG_DOCKER_INSTALL_USER@localhost" "$remote_command"
         else
             ssh -i "${private_path}${install_user_key}" -o StrictHostKeyChecking=no "$CFG_DOCKER_INSTALL_USER@localhost" "$remote_command" > /dev/null 2>&1
+        fi
+    else
+        if [ -z "$silent_flag" ]; then
+            sshpass -p "$CFG_DOCKER_INSTALL_PASS" ssh -o StrictHostKeyChecking=no "$CFG_DOCKER_INSTALL_USER@localhost" "$remote_command"
+        else
+            sshpass -p "$CFG_DOCKER_INSTALL_PASS" ssh -o StrictHostKeyChecking=no "$CFG_DOCKER_INSTALL_USER@localhost" "$remote_command" > /dev/null 2>&1
         fi
     fi
 
