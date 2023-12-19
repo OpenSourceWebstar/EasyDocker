d037ed9f1a7b184b6d43215cadbd890e2f1854ed
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-12-19 19:20:18 +0000

    * Fixed issue with SSH setup being triggered when there is no need.

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -104,6 +104,7 @@ checkRequirements()
 			isSuccessful "The SSH Key(s) for root appears to be setup."
 		else
 			isNotice "An SSH Key for root is not setup."
+			SSHKEY_SETUP_NEEDED="true"
 			((preinstallneeded++))
 		fi
 	fi
@@ -112,6 +113,7 @@ checkRequirements()
 			isSuccessful "The SSH Key(s) for $sudo_user_name appears to be setup."
 		else
 			isNotice "An SSH Key for $sudo_user_name is not setup."
+			SSHKEY_SETUP_NEEDED="true"
 			((preinstallneeded++))
 		fi
 	fi
@@ -120,6 +122,7 @@ checkRequirements()
 			isSuccessful "The SSH Key(s) for $CFG_DOCKER_INSTALL_USER appears to be setup."
 		else
 			isNotice "An SSH Key for $CFG_DOCKER_INSTALL_USER is not setup."
+			SSHKEY_SETUP_NEEDED="true"
 			((preinstallneeded++))
 		fi
 	fi
@@ -128,6 +131,7 @@ checkRequirements()
 			isSuccessful "SSH Password appears to be disabled."
 		else
 			isNotice "Password Authentication has not been disabled."
+			SSHKEY_SETUP_NEEDED="true"
 			((preinstallneeded++))
 		fi
 	fi
--- scripts/install/install_ssh.sh
+++ scripts/install/install_ssh.sh
@@ -255,38 +255,40 @@ updateSSHPermissions()
 installSSHKeysForDownload()
 {
     local flag="$1"
-    echo ""
-    echo "############################################"
-    echo "######        SSH Key Install         ######"
-    echo "############################################"
-    echo ""
+    if [[ "$SSHKEY_SETUP_NEEDED" == "true" ]]; then
+        echo ""
+        echo "############################################"
+        echo "######        SSH Key Install         ######"
+        echo "############################################"
+        echo ""
 
-    ssh_new_key="false"
+        ssh_new_key="false"
 
-    # Fix permissions for SSH Directory
-    local result=$(sudo chmod 0775 "$ssh_dir" > /dev/null 2>&1)
-    checkSuccess "Updating $ssh_dir with 0775 permissions."
-    local result=$(sudo chown $CFG_DOCKER_INSTALL_USER:$CFG_DOCKER_INSTALL_USER "$ssh_dir" > /dev/null 2>&1)
-    checkSuccess "Updating $ssh_dir with $CFG_DOCKER_INSTALL_USER ownership."
+        # Fix permissions for SSH Directory
+        local result=$(sudo chmod 0775 "$ssh_dir" > /dev/null 2>&1)
+        checkSuccess "Updating $ssh_dir with 0775 permissions."
+        local result=$(sudo chown $CFG_DOCKER_INSTALL_USER:$CFG_DOCKER_INSTALL_USER "$ssh_dir" > /dev/null 2>&1)
+        checkSuccess "Updating $ssh_dir with $CFG_DOCKER_INSTALL_USER ownership."
 
-    # Check if SSH Keys are enabled
-    if [[ "$CFG_REQUIREMENT_SSHKEY_ROOT" == "true" ]]; then
-        generateSSHSetupKeyPair "root" $flag
-    fi
-    if [[ "$CFG_REQUIREMENT_SSHKEY_ROOT" == "true" ]]; then
-        generateSSHSetupKeyPair "$sudo_user_name" $flag
-    fi
-    if [[ "$CFG_REQUIREMENT_SSHKEY_ROOT" == "true" ]]; then
-        generateSSHSetupKeyPair "$CFG_DOCKER_INSTALL_USER" $flag
-    fi
+        # Check if SSH Keys are enabled
+        if [[ "$CFG_REQUIREMENT_SSHKEY_ROOT" == "true" ]]; then
+            generateSSHSetupKeyPair "root" $flag
+        fi
+        if [[ "$CFG_REQUIREMENT_SSHKEY_ROOT" == "true" ]]; then
+            generateSSHSetupKeyPair "$sudo_user_name" $flag
+        fi
+        if [[ "$CFG_REQUIREMENT_SSHKEY_ROOT" == "true" ]]; then
+            generateSSHSetupKeyPair "$CFG_DOCKER_INSTALL_USER" $flag
+        fi
 
-    # Install if keys have been setup
-    if [[ "$ssh_new_key" == "true" ]]; then
-        installApp sshdownload;
-    fi
+        # Install if keys have been setup
+        if [[ "$ssh_new_key" == "true" ]]; then
+            installApp sshdownload;
+        fi
 
-    if [[ "$CFG_REQUIREMENT_SSH_DISABLE_PASSWORDS" == "true" ]]; then
-        disableSSHPasswords;
+        if [[ "$CFG_REQUIREMENT_SSH_DISABLE_PASSWORDS" == "true" ]]; then
+            disableSSHPasswords;
+        fi
     fi
 }
 
