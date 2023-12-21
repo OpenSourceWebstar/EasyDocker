33458d6e44745339dc852362dad829b0a788caae
OpenSourceWebstar <info@scottwebstar.co.uk>
2023-12-21 06:39:44 +0000

    * Updated WireGuard with /32 for address.

--- scripts/install/install_wireguard.sh
+++ scripts/install/install_wireguard.sh
@@ -40,7 +40,7 @@ installStandaloneWireGuard()
                     sudo apt-get install -y wireguard iptables resolvconf qrencode
 
                     # Update DNS after installing resolvconf
-                    updateDNS;
+                    updateDNS "" standalonewireguard;
 
                     # Check if the directory exists; if not, create it
                     if [ ! -d "/etc/wireguard" ]; then
@@ -71,7 +71,7 @@ ALLOWED_IPS=${CFG_WG_ALLOWED_IPS}" | sudo tee /etc/wireguard/params >/dev/null
 
                     # Add server interface
                     echo "[Interface]
-Address = ${CFG_WG_SERVER_IPV4}/24
+Address = ${CFG_WG_SERVER_IPV4}/32
 ListenPort = ${CFG_WG_SERVER_PORT}
 PrivateKey = ${SERVER_PRIV_KEY}" | sudo tee "/etc/wireguard/${CFG_WG_SERVER_NIC}.conf" >/dev/null
 
@@ -180,7 +180,7 @@ wireguardNewClient()
     # Create client file and add the server as a peer
     echo "[Interface]
 PrivateKey = ${WIREGUARD_CLIENT_PRIV_KEY}
-Address = ${CFG_WG_SERVER_IPV4}/24
+Address = ${CFG_WG_SERVER_IPV4}/32
 DNS = ${CFG_DNS_SERVER_1},${CFG_DNS_SERVER_2}
 
 [Peer]
@@ -196,10 +196,13 @@ PublicKey = ${WIREGUARD_CLIENT_PUB_KEY}
 PresharedKey = ${WIREGUARD_CLIENT_PRE_SHARED_KEY}
 AllowedIPs = ${CFG_WG_ALLOWED_IPS}" | sudo tee -a "/etc/wireguard/${CFG_WG_SERVER_NIC}.conf" >/dev/null
 
-    result=$(sudo systemctl stop "wg-quick@${CFG_WG_SERVER_NIC}")
-    checkSuccess "Stopped wg-quick@${CFG_WG_SERVER_NIC} service."
-    result=$(sudo systemctl start "wg-quick@${CFG_WG_SERVER_NIC}")
-    checkSuccess "Started wg-quick@${CFG_WG_SERVER_NIC} service."
+    #result=$(sudo wg syncconf ${CFG_WG_SERVER_NIC} /etc/wireguard/${CFG_WG_SERVER_NIC}.conf)
+    #checkSuccess "Syncing config file for $CFG_WG_SERVER_NIC"
+
+    #result=$(sudo systemctl stop "wg-quick@${CFG_WG_SERVER_NIC}")
+    #checkSuccess "Stopped wg-quick@${CFG_WG_SERVER_NIC} service."
+    #result=$(sudo systemctl start "wg-quick@${CFG_WG_SERVER_NIC}")
+    #checkSuccess "Started wg-quick@${CFG_WG_SERVER_NIC} service."
 
     # Generate QR code if qrencode is installed
     if command -v qrencode &>/dev/null; then
