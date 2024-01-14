7a0c768bd07f047df1a4d2a955b2e7ed80acb066
OpenSourceWebstar <info@scottwebstar.co.uk>
2024-01-14 22:19:21 +0000

    * Added some checks for restartApp function.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -337,17 +337,21 @@ installApp()
     ${installFuncName}
 }
 
-restartApp()
+restartApp() 
 {
     local app_name="$1"
     local app_name_ucfirst="$(tr '[:lower:]' '[:upper:]' <<< ${app_name:0:1})${app_name:1}"
     local installFuncName="install${app_name_ucfirst}"
 
-    # Create a variable with the name of $app_name and set its value to "i"
+    # Create a variable with the name of $app_name and set its value to "r"
     declare "${app_name}=r"
 
-    # Call the installation function
-    ${installFuncName}
+    # Check if the installation function exists before calling it
+    if [ -n "$(type -t ${installFuncName})" ] && [ "$(type -t ${installFuncName})" = function ]; then
+        ${installFuncName}
+    else
+        isNotice "Installation function ${installFuncName} not found."
+    fi
 }
 
 setupComposeFile()
