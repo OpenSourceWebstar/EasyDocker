822734ccaaf3a4291fb0312a5bb889e96c1cebfa
OpenSourceWebstar <scottwebstar@proton.me>
2024-01-30 22:35:14 +0000

    * Switched to a sudo check for if the sockets exist.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -957,17 +957,17 @@ dockerSetSocketPermissions()
 
     if [[ $CFG_DOCKER_INSTALL_TYPE == "root" ]]; then
         # if File exists
-        if [ -e "$docker_rootless_socket" ]; then
+        if sudo test -e "$docker_rootless_socket"; then
             local result=$(sudo chmod o-r "$docker_rootless_socket")
             checkSuccess "Removing read permissions from Rootless docker socket." "$result"
             docker_rootless_found="true"
         else
-            isSuccessful "Rootless socket not found, no need to do anything with rootless setup."
+            #isSuccessful "Rootless socket not found, no need to do anything with rootless setup."
             docker_rootless_found="false"
         fi
 
         # if File exists
-        if [ -e "$docker_rooted_socket" ]; then
+        if sudo test -e "$docker_rooted_socket"; then
             local result=$(sudo chmod +r "$docker_rooted_socket")
             checkSuccess "Adding read permissions to Rooted docker socket." "$result"
             docker_rooted_found="true"
@@ -979,17 +979,17 @@ dockerSetSocketPermissions()
 
     if [[ $CFG_DOCKER_INSTALL_TYPE == "rootless" ]]; then
         # if File exists
-        if [ -e "$docker_rooted_socket" ]; then
+        if sudo test -e "$docker_rooted_socket"; then
             local result=$(sudo chmod o-r "$docker_rooted_socket")
             checkSuccess "Removing read permissions from Rooted docker socket." "$result"
             docker_rooted_found="true"
         else
-            isSuccessful "Rooted socket not found, no need to do anything with rooted setup."
+            #isSuccessful "Rooted socket not found, no need to do anything with rooted setup."
             docker_rooted_found="false"
         fi
 
         # if File exists
-        if [ -e "$docker_rootless_socket" ]; then
+        if sudo test -e "$docker_rootless_socket"; then
             local result=$(sudo chmod +r "$docker_rootless_socket")
             checkSuccess "Adding read permissions to Rootless docker socket." "$result"
             docker_rootless_found="true"
