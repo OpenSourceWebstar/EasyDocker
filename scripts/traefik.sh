183da5ae51d238509f196cc65c692e327afc2e47
OpenSourceWebstar <scottwebstar@proton.me>
2024-02-09 12:52:49 +0000

    * Fixed login credentials for Traefik setup.

--- scripts/traefik.sh
+++ scripts/traefik.sh
@@ -90,11 +90,11 @@ traefikSetupLoginCredentials()
 	local protectionauth_file="$containers_dir/traefik/etc/dynamic/middlewears/protectionauth.yml"
     if [ -f "$protectionauth_file" ]; then
 		# Setup BasicAuth credentials
-		local password_hash=$(htpasswd -Bbn "$CFG_LOGIN_USER" "$CFG_LOGIN_PASS")
+		local login_credentials=$(htpasswd -Bbn "$CFG_LOGIN_USER" "$CFG_LOGIN_PASS")
 
         local result=$(sudo sed -i '/#protection credentials/d' "$protectionauth_file")
         checkSuccess "Delete the line containing protection credentials"
-        local result=$(sudo sed -i "/users:/a\\          - '$CFG_LOGIN_USER:$password_hash' #protection credentials" "$protectionauth_file")
+        local result=$(sudo sed -i "/users:/a\\          - '$login_credentials' #protection credentials" "$protectionauth_file")
         checkSuccess "Add the new line with new protection credentials"
 	fi
 }
