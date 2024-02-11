cdbf8f9008ebb1cdf01531e8d68f6b29a0c35d45
OpenSourceWebstar <scottwebstar@proton.me>
2024-02-11 14:32:40 +0000

    * Added easydocker update CLI command.

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -30,6 +30,7 @@ cliShowCommands()
     echo "Available Commands:"
     echo ""
     echo "  easydocker run                          - Run the EasyDocker control panel"
+    echo "  easydocker update                       - Updates EasyDocker to the latest version"
     echo "  easydocker reset                        - Reinstall EasyDocker install files"
     echo "  easydocker app [name] [action]          - Manage apps in EasyDocker"
     echo "  easydocker dockertype [type]            - Set the Docker type"
