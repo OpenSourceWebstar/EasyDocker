2d812f365e6431dc83d958f572c9e893741d2536
OpenSourceWebstar <scottwebstar@proton.me>
2024-02-18 19:09:52 +0000

    * Added docker type for CLI specific variable.

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -123,15 +123,15 @@ cliListDockertypeCommands()
 {
     # Select preexisting docker_type
     if [ -f "$docker_dir/$db_file" ]; then
-        local docker_type=$(sudo sqlite3 "$docker_dir/$db_file" 'SELECT content FROM options WHERE option = "docker_type";')
+        local docker_type_cli=$(sudo sqlite3 "$docker_dir/$db_file" 'SELECT content FROM options WHERE option = "docker_type";')
         # Insert into DB if something doesnt exist
-        if [[ $docker_type == "" ]]; then
+        if [[ $docker_type_cli == "" ]]; then
             databaseOptionInsert "docker_type" $CFG_DOCKER_INSTALL_TYPE;
-            local docker_type=$(sudo sqlite3 "$docker_dir/$db_file" 'SELECT content FROM options WHERE option = "docker_type";')
+            local docker_type_cli=$(sudo sqlite3 "$docker_dir/$db_file" 'SELECT content FROM options WHERE option = "docker_type";')
         fi
     fi
     echo ""
-    isNotice "The current Docker Setup Type is currently : ${RED}$docker_type${NC}" 
+    isNotice "The current Docker Setup Type is currently : ${RED}$docker_type_cli${NC}" 
     echo ""
     echo "Available Dockertype Commands:"
     echo ""
