89de32625fe7de849109580c61afb2062ee81b8e
OpenSourceWebstar <scottwebstar@proton.me>
2024-02-19 08:49:41 +0000

    * Updated the dockerDownShutdown function to no longer require an OS Check.

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -25,30 +25,29 @@ dockerDownShutdown()
     local app_name="$1"
     local type="$2"
 
-    #if [ -e $containers_dir$app_name/docker-compose.yml ]; then
-        if [[ "$OS" == [1234567] ]]; then
-            if [[ "$type" == "" ]]; then
-                # Used for standard app shutdown
-                if [[ $CFG_DOCKER_INSTALL_TYPE == "rootless" ]]; then
-                    local result=$(runCommandForDockerInstallUser "cd $containers_dir$app_name && docker-compose down")
-                    isSuccessful "Shutting down container for $app_name"
-                elif [[ $CFG_DOCKER_INSTALL_TYPE == "rooted" ]]; then
-                    local result=$(cd $containers_dir$app_name && sudo docker-compose down)
-                    isSuccessful "Shutting down container for $app_name"
-                fi
-            else
-                # Used for Shutting down Rooted type switcher
-                if [[ $type == "rootless" ]]; then
-                    local result=$(runCommandForDockerInstallUser "cd $containers_dir$app_name && docker-compose down")
-                    isSuccessful "Shutting down container for $app_name"
-                elif [[ $type == "rooted" ]]; then
-                    local result=$(cd $containers_dir$app_name && sudo docker-compose down)
-                    isSuccessful "Shutting down container for $app_name"
-                fi
-            fi
+    if [[ "$type" == "" ]]; then
+        # Used for standard app shutdown
+        if [[ $CFG_DOCKER_INSTALL_TYPE == "rootless" ]]; then
+            local result=$(runCommandForDockerInstallUser "cd $containers_dir$app_name && docker-compose down")
+            isSuccessful "Shutting down container for $app_name"
+            dockerDownShutdownSuccessMessage $app_name;
+        elif [[ $CFG_DOCKER_INSTALL_TYPE == "rooted" ]]; then
+            local result=$(cd $containers_dir$app_name && sudo docker-compose down)
+            isSuccessful "Shutting down container for $app_name"
+            dockerDownShutdownSuccessMessage $app_name;
         fi
-        dockerDownShutdownSuccessMessage $app_name;
-    #fi
+    else
+        # Used for Shutting down Rooted type switcher
+        if [[ $type == "rootless" ]]; then
+            local result=$(runCommandForDockerInstallUser "cd $containers_dir$app_name && docker-compose down")
+            isSuccessful "Shutting down container for $app_name"
+            dockerDownShutdownSuccessMessage $app_name;
+        elif [[ $type == "rooted" ]]; then
+            local result=$(cd $containers_dir$app_name && sudo docker-compose down)
+            isSuccessful "Shutting down container for $app_name"
+            dockerDownShutdownSuccessMessage $app_name;
+        fi
+    fi
 }
 
 dockerDownShutdownSuccessMessage()
