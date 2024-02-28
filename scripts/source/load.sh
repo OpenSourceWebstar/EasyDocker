27f2fe1ac0132165e5eb08905cbb197c9e47b42d
OpenSourceWebstar <scottwebstar@proton.me>
2024-02-28 18:01:46 +0000

    * Added file source protection for the initial files needed to load.

--- scripts/source/load.sh
+++ scripts/source/load.sh
@@ -2,11 +2,27 @@
 
 # This is used for initial loading
 # The starting point and is the only code that doesnt contain logic requirements
-source "init.sh"
-source "variables.sh"
-source "${install_scripts_dir}source/loading/check.sh"
-source "${install_scripts_dir}source/loading/initilize.sh"
-source "${install_scripts_dir}source/loading/scan.sh"
+
+# Source files if they exist, otherwise return an error
+if [ -f "init.sh" ] && \
+   [ -f "variables.sh" ] && \
+   [ -f "${install_scripts_dir}source/loading/check.sh" ] && \
+   [ -f "${install_scripts_dir}source/loading/initilize.sh" ] && \
+   [ -f "${install_scripts_dir}source/loading/scan.sh" ]; then
+    source "init.sh"
+    source "variables.sh"
+    source "${install_scripts_dir}source/loading/check.sh"
+    source "${install_scripts_dir}source/loading/initilize.sh"
+    source "${install_scripts_dir}source/loading/scan.sh"
+else
+    # Print an error message for any missing files
+    [ ! -f "init.sh" ] && echo "Error: File 'init.sh' does not exist. Unable to source."
+    [ ! -f "variables.sh" ] && echo "Error: File 'variables.sh' does not exist. Unable to source."
+    [ ! -f "${install_scripts_dir}source/loading/check.sh" ] && echo "Error: File '${install_scripts_dir}source/loading/check.sh' does not exist. Unable to source."
+    [ ! -f "${install_scripts_dir}source/loading/initilize.sh" ] && echo "Error: File '${install_scripts_dir}source/loading/initilize.sh' does not exist. Unable to source."
+    [ ! -f "${install_scripts_dir}source/loading/scan.sh" ] && echo "Error: File '${install_scripts_dir}source/loading/scan.sh' does not exist. Unable to source."
+    return 1
+fi
 
 # For starting the script
 if [[ $init_run_flag == "true" ]]; then
