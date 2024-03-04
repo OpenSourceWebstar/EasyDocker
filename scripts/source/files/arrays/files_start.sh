985af09bd19d517888725e8eda4731a7c8908590
OpenSourceWebstar <scottwebstar@proton.me>
2024-03-04 23:35:40 +0000

    * Removed unneded logo file from start.

--- scripts/source/files/arrays/files_start.sh
+++ scripts/source/files/arrays/files_start.sh
@@ -5,7 +5,6 @@ start_scripts=(
     "start/start_exit.sh"
     "start/start_install.sh"
     "start/start_load.sh"
-    "start/start_logo.sh"
     "start/start_other.sh"
     "start/start_preinstall.sh"
     "start/start_recommended.sh"
