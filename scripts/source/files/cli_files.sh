1c2ff57be6e4829fa01947b72d1952cb9f7c6473
OpenSourceWebstar <scottwebstar@proton.me>
2024-03-04 23:33:24 +0000

    * Added start files to loading arrays.

--- scripts/source/files/cli_files.sh
+++ scripts/source/files/cli_files.sh
@@ -1,38 +1,12 @@
 #!/bin/bash
 
-# Used for the full EasyDocker application
-files_easydocker_app=(
-    "${app_scripts[@]}"
-    "${backup_scripts[@]}"
-    "${checks_scripts[@]}"
-    "${cli_scripts[@]}"
-    "${config_scripts[@]}"
-    "${crontab_scripts[@]}"
-    "${database_scripts[@]}"
-    "${docker_scripts[@]}"
-    "${function_scripts[@]}"
-    "${logs_scripts[@]}"
-    "${menu_scripts[@]}"
-    "${migrate_scripts[@]}"
-    "${network_scripts[@]}"
-    "${os_scripts[@]}"
-    "${restore_scripts[@]}"
-    "${source_scripts[@]}"
-    "${ssh_scripts[@]}"
-    "${ssl_scripts[@]}"
-    "${swapfile_scripts[@]}"
-    "${ufw_scripts[@]}"
-    "${ufwd_scripts[@]}"
-    "${update_scripts[@]}"
-    "${wireguard_scripts[@]}"
-)
-
 # Used for the cli
 files_easydocker_cli=(
     "${app_scripts[@]}"
     "${backup_scripts[@]}"
     "${checks_scripts[@]}"
     "${cli_scripts[@]}"
+    "${command_scripts[@]}"
     "${config_scripts[@]}"
     "${crontab_scripts[@]}"
     "${database_scripts[@]}"
@@ -47,6 +21,7 @@ files_easydocker_cli=(
     "${source_scripts[@]}"
     "${ssh_scripts[@]}"
     "${ssl_scripts[@]}"
+    "${start_scripts[@]}"
     "${swapfile_scripts[@]}"
     "${ufw_scripts[@]}"
     "${ufwd_scripts[@]}"
