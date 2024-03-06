4431aae4d7d2f2dff535665dc5b093606ba847c1
OpenSourceWebstar <scottwebstar@proton.me>
2024-03-06 10:56:19 +0000

    * Fixed issue with syntax quotes for pruning docker networks.

--- scripts/docker/network/prune_networks.sh
+++ scripts/docker/network/prune_networks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-dockerPruneNetworks()
+dockerPruneNetworks() 
 {
-    local result=$(dockerCommandRun "docker network prune -f --filter \"name!=vpn\"")
+    local result=$(dockerCommandRun "docker network prune -f --filter 'name!=vpn'")
     checkSuccess "Pruning unused Docker networks (excluding vpn)"
 }
