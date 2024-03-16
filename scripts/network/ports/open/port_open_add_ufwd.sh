78389556145263bea2c146de17e5dc0d44396acd
OpenSourceWebstar <scottwebstar@proton.me>
2024-03-16 15:18:13 +0000

    * Removed ufwd_port_array from portOpenUfwd function.

--- scripts/network/ports/open/port_open_add_ufwd.sh
+++ scripts/network/ports/open/port_open_add_ufwd.sh
@@ -6,11 +6,5 @@ portOpenUfwd()
     local port="$2"
     local type="$3"
     
-    # Check if ufwd_port_array is unset or null
-    if [[ -z "${ufwd_port_array[@]}" ]]; then
-        # If unset or null, declare the array
-        declare -a ufwd_port_array=()
-    fi
-    
     ufwd_port_array+=("$app_name:$port/$type")
 }
