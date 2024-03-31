8fbf46b9b51717293e25dbf845afd360e883b1a5
OpenSourceWebstar <scottwebstar@proton.me>
2024-03-31 15:27:59 +0100

    * Removed rootless check for setting up the rootless user.

--- scripts/docker/install/rootless/rootless_user.sh
+++ scripts/docker/install/rootless/rootless_user.sh
@@ -2,14 +2,12 @@
 
 installDockerRootlessUser()
 {   
-    if [[ $CFG_DOCKER_INSTALL_TYPE == "rootless" ]]; then
-        if id "$CFG_DOCKER_INSTALL_USER" &>/dev/null; then
-            isSuccessful "User $CFG_DOCKER_INSTALL_USER already exists."
-        else
-            # If the user doesn't exist, create the user
-            local result=$(sudo useradd -s /bin/bash -d "/home/$CFG_DOCKER_INSTALL_USER")
-            checkSuccess "Creating $CFG_DOCKER_INSTALL_USER User."
-            updateDockerInstallPassword;
-        fi
+    if id "$CFG_DOCKER_INSTALL_USER" &>/dev/null; then
+        isSuccessful "User $CFG_DOCKER_INSTALL_USER already exists."
+    else
+        # If the user doesn't exist, create the user
+        local result=$(sudo useradd -s /bin/bash -d "/home/$CFG_DOCKER_INSTALL_USER")
+        checkSuccess "Creating $CFG_DOCKER_INSTALL_USER User."
+        updateDockerInstallPassword;
     fi
 }
