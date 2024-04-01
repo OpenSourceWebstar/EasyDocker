f5f07b67490be2468e3cc58d7c67d1fe3bc5dd85
OpenSourceWebstar <scottwebstar@proton.me>
2024-04-01 16:08:03 +0100

    * Added more crontab checks to stop the install running when not needed.

--- scripts/checks/requirements/check_crontab.sh
+++ scripts/checks/requirements/check_crontab.sh
@@ -6,8 +6,10 @@ checkCrontabRequirement()
 		### Crontab
 		if [[ "$ISCRON" != *"command not found"* ]] && sudo -u $sudo_user_name crontab -l 2>/dev/null | grep -q "cron is set up for $sudo_user_name"; then
 			isSuccessful "Crontab is successfully set up."
+			CRONTAB_SETUP="true"
 		else
 			isNotice "Crontab not installed. Setup will start soon."
+			CRONTAB_SETUP="false"
 			((preinstallneeded++))
 		fi
 	fi
