d82dc54587d1ec2c67118f331fe354f527cfc2e1
OpenSourceWebstar <scottwebstar@proton.me>
2024-04-01 15:49:50 +0100

    * Added reload group after adding easydocker user to the docker group so that there are no issues with accessing the socket on setup.

--- scripts/docker/service/start_docker.sh
+++ scripts/docker/service/start_docker.sh
@@ -11,6 +11,9 @@ dockerServiceStart()
 
         local result=$(sudo usermod -aG docker $sudo_user_name)
         checkSuccess "Adding user to 'docker' group"
+
+        local result=$(sudo newgrp docker)
+        checkSuccess "Reloading docker group for $sudo_user_name"
     elif [[ $CFG_DOCKER_INSTALL_TYPE == "rootless" ]]; then
         installDockerRootless;
     fi
