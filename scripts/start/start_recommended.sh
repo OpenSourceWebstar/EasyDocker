7ca8d44f5c3cdfe4e48b965b92a19c5b98a07276
OpenSourceWebstar <scottwebstar@proton.me>
2024-04-04 22:14:51 +0100

    * Added wireguard to the recommended installation  apps.

--- scripts/start/start_recommended.sh
+++ scripts/start/start_recommended.sh
@@ -2,6 +2,35 @@
 
 installRecommendedApps()
 {
+    local wireguard_status=$(dockerCheckAppInstalled "wireguard" "docker")
+    if [[ "$wireguard_status" != "installed" ]]; then
+        echo ""
+        echo "####################################################"
+        echo "###     Recommended Application - Wireguard      ###"
+        echo "####################################################"
+        echo ""
+        isNotice "It's recommended to install Wireguard upon first install."
+        echo ""
+        isNotice "Wireguard allows you to access the server via VPN."
+        echo ""
+        while true; do
+            isQuestion "Would you like to Wireguard as per the recommendation? (y/n): "
+            read -p "" wireguard_choice
+            if [[ -n "$wireguard_choice" ]]; then
+                break
+            fi
+            isNotice "Please provide a valid input."
+        done
+        if [[ "$wireguard_choice" == [yY] ]]; then
+            if [[ "$wireguard_status" != "installed" ]]; then
+                dockerInstallApp wireguard;
+            fi
+
+        elif [[ "$wireguard_choice" == [nN] ]]; then
+            break
+        fi
+    fi
+
     # Install if keys have been setup
     if [[ "$ssh_new_key" == "true" ]]; then
         echo ""
@@ -9,15 +38,16 @@ installRecommendedApps()
         echo "###              SSH Key Downloader              ###"
         echo "####################################################"
         echo ""
-        isNotice "As new SSH Keys have been setup, the key download app will be installed now..."
+        isNotice "As new SSH Keys have been setup, the key downloader app will be installed now..."
         echo ""
         dockerInstallApp sshdownload;
     fi
+
     local traefik_status=$(dockerCheckAppInstalled "traefik" "docker")
     if [[ "$traefik_status" != "installed" ]]; then
         echo ""
         echo "####################################################"
-        echo "###           Recommended Applications           ###"
+        echo "###      Recommended Application - Traefik       ###"
         echo "####################################################"
         echo ""
         isNotice "It's recommended to install Traefik upon first install."
@@ -25,7 +55,7 @@ installRecommendedApps()
         isNotice "Traefik secures your Network traffic and automatically installs SSL Certificates"
         echo ""
         while true; do
-            isQuestion "Would you like to follow the recommendations? (y/n): "
+            isQuestion "Would you like to Traefik as per the recommendation? (y/n): "
             read -p "" recommendation_choice
             if [[ -n "$recommendation_choice" ]]; then
                 break
@@ -35,17 +65,18 @@ installRecommendedApps()
         if [[ "$recommendation_choice" == [yY] ]]; then
             # Traefik
             if [[ "$traefik_status" != "installed" ]]; then
-                traefik=i
-                installTraefik;
+                dockerInstallApp traefik;
             fi
 
             isSuccessful "All recommended apps have successfully been set up."
         elif [[ "$recommendation_choice" == [nN] ]]; then
-            local general_config_file="$configs_dir$config_file_general"
-            result=$(sudo sed -i "s|CFG_REQUIREMENT_SUGGEST_INSTALLS=true|CFG_REQUIREMENT_SUGGEST_INSTALLS=false|" "$general_config_file")
-            checkSuccess "Disabling install recommendations in the requirements config."
-            isNotice "You can re-enable this in the requirements config file"
-            sourceScanFiles "easydocker_configs";
+            break
         fi
     fi
+
+    #local general_config_file="$configs_dir$config_file_general"
+    #result=$(sudo sed -i "s|CFG_REQUIREMENT_SUGGEST_INSTALLS=true|CFG_REQUIREMENT_SUGGEST_INSTALLS=false|" "$general_config_file")
+    #checkSuccess "Disabling install recommendations in the requirements config."
+    #isNotice "You can re-enable this in the requirements config file"
+    #sourceScanFiles "easydocker_configs";
 }
