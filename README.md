74a924fd992da1c57a8c5a0e42ca39ea42354e10
OpenSourceWebstar <scottwebstar@proton.me>
2024-04-09 10:25:56 +0100

    * Added NextCloud to the list of applications available.

--- README.md
+++ README.md
@@ -55,6 +55,7 @@ Ultimately, an automated Docker script is a powerful solution that streamlines t
 | User           | <a href="https://github.com/killbill/killbill">Killbill - Payment Processing</a>                    | Tested & Working |
 | User           | <a href="https://github.com/mattermost/mattermost">Mattermost - Collaboration Platform</a>          | Tested & Working |
 | User           | <a href="https://github.com/kimai/kimai">Kimai - Online-Timetracker</a>                             | Tested & Working |
+| User           | <a href="https://github.com/nextcloud/docker">NextCloud - Business Cloud Platform</a>               | Tested & Working |
 | User           | <a href="https://github.com/Tiledesk/tiledesk">Tiledesk - Live Chat Platform</a> *UNFINISHED*       | Needs Finishing  |
 | User           | <a href="https://github.com/rustdesk/rustdesk/">Rustdesk - Remote Desktop Server</a>                | Needs Testing    |
 | User           | <a href="https://gitlab.com/gitlab-org/gitlab">GitLab - DevOps Platform</a> *UNFINISHED*            | Needs Testing    |
---
+++ containers/nextcloud/nextcloud.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+# Category : system
+# Description : Nextcloud - Business Cloud Platform (c/t/u/s/r/i):
+
+installNextcloud()
+{
+    if [[ "$nextcloud" == *[cCtTuUsSrRiI]* ]]; then
+        dockerConfigSetupToContainer silent nextcloud;
+        local app_name=$CFG_NEXTCLOUD_APP_NAME
+		setupInstallVariables $app_name;
+    fi
+
+    if [[ "$nextcloud" == *[cC]* ]]; then
+        editAppConfig $app_name;
+    fi
+
+    if [[ "$nextcloud" == *[uU]* ]]; then
+        dockerUninstallApp $app_name;
+    fi
+
+    if [[ "$nextcloud" == *[sS]* ]]; then
+        dockerComposeDown $app_name;
+    fi
+
+    if [[ "$nextcloud" == *[rR]* ]]; then
+        dockerComposeRestart $app_name;
+    fi
+
+    if [[ "$nextcloud" == *[iI]* ]]; then
+        echo ""
+        echo "##########################################"
+        echo "###          Install $app_name"
+        echo "##########################################"
+        echo ""
+
+		((menu_number++))
+        echo ""
+        echo "---- $menu_number. Setting up install folder and config file for $app_name."
+        echo ""
+
+        dockerConfigSetupToContainer "loud" "$app_name" "install";
+        isSuccessful "Install folders and Config files have been setup for $app_name."
+
+        ((menu_number++))
+        echo ""
+        echo "---- $menu_number. Checking & Opening ports if required"
+        echo ""
+
+        portsCheckApp $app_name install;
+        if [[ $disallow_used_port == "true" ]]; then
+            isError "A used port conflict has occured, setup is cancelling..."
+            disallow_used_port=""
+            return
+        else
+            isSuccessful "No used port conflicts found, setup is continuing..."
+        fi
+        if [[ $disallow_open_port == "true" ]]; then
+            isError "An open port conflict has occured, setup is cancelling..."
+            disallow_open_port=""
+            return
+        else
+            isSuccessful "No open port conflicts found, setup is continuing..."
+        fi
+        
+		((menu_number++))
+        echo ""
+        echo "---- $menu_number. Setting up the $app_name docker-compose.yml file."
+        echo ""
+
+        dockerComposeSetupFile $app_name;
+
+		((menu_number++))
+        echo ""
+        echo "---- $menu_number. Updating file permissions before starting."
+        echo ""
+
+		fixPermissionsBeforeStart $app_name;
+
+		((menu_number++))
+        echo ""
+        echo "---- $menu_number. Running the docker-compose.yml to install and start $app_name"
+        echo ""
+
+		dockerComposeUpdateAndStartApp $app_name install;
+
+        ((menu_number++))
+        echo ""
+        echo "---- $menu_number. Running Application specific updates (if required)"
+        echo ""
+
+        appUpdateSpecifics $app_name;
+
+		((menu_number++))
+        echo ""
+        echo "---- $menu_number. Running Headscale setup (if required)"
+        echo ""
+
+		setupHeadscale $app_name;
+
+		((menu_number++))
+		echo ""
+        echo "---- $menu_number. Adding $app_name to the Apps Database table."
+        echo ""
+
+		databaseInstallApp $app_name;
+
+		((menu_number++))
+        echo ""
+        echo "---- $menu_number. You can find $app_name files at $containers_dir$app_name"
+        echo ""
+        echo "    You can now navigate to your new service using one of the options below : "
+        echo ""
+        
+        menuShowFinalMessages $app_name;
+		    
+		menu_number=0
+        sleep 3s
+        cd
+    fi
+    nextcloud=n
+}
