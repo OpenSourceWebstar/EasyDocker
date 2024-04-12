b3ff9d4d256f15b685fbeeb5127b40a52f2367fb
OpenSourceWebstar <scottwebstar@proton.me>
2024-04-12 17:46:56 +0100

    * Fixed issue with pathing for backup entrys.

--- scripts/crontab/app/install/setup.sh
+++ scripts/crontab/app/install/setup.sh
@@ -18,9 +18,9 @@ installSetupCrontab()
     fi
 
     if [ "$entry_name" = "full" ]; then
-        local crontab_entry="$CFG_BACKUP_CRONTAB_FULL /docker/install/start.sh app backup $entry_name"
+        local crontab_entry="$CFG_BACKUP_CRONTAB_FULL cd /docker/install/ && ./start.sh app backup $entry_name >> /docker/logs/backup.log 2>&1"
     else
-        local crontab_entry="$CFG_BACKUP_CRONTAB_APP /docker/install/start.sh app backup $entry_name"
+        local crontab_entry="$CFG_BACKUP_CRONTAB_APP cd /docker/install/ && ./start.sh app backup $entry_name >> /docker/logs/backup.log 2>&1"
     fi
 
     local apps_comment="# CRONTAB BACKUP APPS"
