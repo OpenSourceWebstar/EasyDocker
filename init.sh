ac6d15ff1e316e3d360dac9ff7866510e6c39b08
OpenSourceWebstar <scottwebstar@proton.me>
2024-04-12 17:58:43 +0100

    * Fix issue where the sudo entry was not able to find an already existing one.

--- init.sh
+++ init.sh
@@ -107,7 +107,7 @@ initializeScript()
 		fi
 		local sudoers_file="/etc/sudoers"
 		# Check if the sudo entry exists for the user
-		if ! sudo grep -qE "^$sudo_user_name\s+ALL=(ALL) NOPASSWD: ALL$" "$sudoers_file"; then
+		if ! sudo grep -qE "$sudo_user_name ALL=(ALL) NOPASSWD: ALL" "$sudoers_file"; then
 			# Add the passwordless sudo entry for the user
 			echo "" | sudo tee -a "$sudoers_file" > /dev/null
 			echo "$sudo_user_name ALL=(ALL) NOPASSWD: ALL" | sudo tee -a "$sudoers_file" > /dev/null
