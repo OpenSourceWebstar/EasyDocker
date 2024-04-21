01603e8792276ca048ec7da52661ee4961bb8d3c
OpenSourceWebstar <scottwebstar@proton.me>
2024-04-21 21:27:36 +0100

    * Tidy up mattermost tool update pass code.

--- scripts/app/containers/app_mattermost.sh
+++ scripts/app/containers/app_mattermost.sh
@@ -27,7 +27,7 @@ appMattermostResetUserPassword()
 
     if [[ "$mattermostusername" != [xX] && "$mattermostpassword" != [xX] ]]; then
         local config_json="$containers_dir/mattermost/volumes/app/mattermost/config/config.json"
-        
+
         # Enable local mode
         result=$(sudo sed -i "s|\"EnableLocalMode\": false|\"EnableLocalMode\": true|" "$config_json")
         checkSuccess "EnableLocalMode set to true for password update."
@@ -36,11 +36,8 @@ appMattermostResetUserPassword()
         isNotice "Waiting 10 seconds for mattermost to load the local socket"
         sleep 10
         # Update Password
-        if [[ $CFG_DOCKER_INSTALL_TYPE == "rootless" ]]; then
-            dockerCommandRunInstallUser "docker exec mattermost /bin/bash -c 'mmctl --local user change-password $mattermostusername --password $mattermostpassword && exit'"
-        elif [[ $CFG_DOCKER_INSTALL_TYPE == "rooted" ]]; then
-            docker exec mattermost /bin/bash -c 'mmctl --local user change-password $mattermostusername --password $mattermostpassword && exit'
-        fi
+        dockerCommandRun "docker exec mattermost /bin/bash -c 'mmctl --local user change-password $mattermostusername --password $mattermostpassword && exit'"
+
         # Disable local mode
         result=$(sudo sed -i "s|\"EnableLocalMode\": true|\"EnableLocalMode\": false|" "$config_json")
         checkSuccess "EnableLocalMode set to false for password update."
