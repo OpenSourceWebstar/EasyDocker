42380468cf338cb6f8ad22c4b9803b37c8746eef
OpenSourceWebstar <scottwebstar@proton.me>
2024-06-30 19:02:11 +0100

    * Removed unneded code from hostname generator.

--- scripts/app/app_generate.sh
+++ scripts/app/app_generate.sh
@@ -140,29 +140,10 @@ appGenerate()
         checkSuccess "Updating Config - CFG_TEMPLATE_HOST_NAME to $app_name"
 
         # Hostfile addition
-        # Get the last IP from the file
-        hostfile_last_ip=$(tail -n 1 "$configs_dir$ip_file" | awk '{print $2}')
-
-        # Check if hostfile_last_ip is not empty or not correctly fetched
-        if [ -z "$hostfile_last_ip" ]; then
-        echo "Error: Unable to fetch last IP from $configs_dir$ip_file"
-        exit 1
-        fi
-
-        # Get the last IP from the file
         last_ip=$(awk '{print $2}' "$configs_dir$ip_file" | tail -n 1)
-
-        # Check if last_ip is not empty
-        if [ -z "$last_ip" ]; then
-            echo "Error: Unable to fetch last IP from $configs_dir$ip_file"
-            exit 1
-        fi
-
         # Split the IP address into parts and increment the last octet
         IFS='.' read -r a b c d <<< "$last_ip"
         new_ip="$a.$b.$c.$((d + 1))"
-
-        # Append the new entry to the file
         echo "$host_name $new_ip" >> "$install_configs_dir$ip_file"
         checkSuccess "Add the new entry to ips_hostname file."
 
