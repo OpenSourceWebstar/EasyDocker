b965a72feadee84744b54f20f24a44676cc22dc0
OpenSourceWebstar <scottwebstar@proton.me>
2024-06-30 13:17:29 +0100

    * Added new RANDOMIZEDPASSWORD 1 to 9 for generating single passwords.

--- scripts/config/password/update_file.sh
+++ scripts/config/password/update_file.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
 
-scanFileForRandomPassword() 
+scanFileForRandomPassword()
 {
     local file="$1"
     
+    # Declare an array to hold passwords for placeholders 1 to 9
+    local passwords=()
+    
     if [ -f "$file" ]; then
-        # Check if the file contains the placeholder string "RANDOMIZEDPASSWORD"
-        while sudo grep  -q "RANDOMIZEDPASSWORD" "$file"; do
-            # Generate a unique random password
-            local random_password=$(openssl rand -base64 12 | tr -d '+/=')
+        # First, handle placeholders RANDOMIZEDPASSWORD1 to RANDOMIZEDPASSWORD9
+        for i in {1..9}; do
+            local placeholder="RANDOMIZEDPASSWORD${i}"
             
-            # Find the line number of the first occurrence of "RANDOMIZEDPASSWORD"
-            local line_number=$(sudo grep -n "RANDOMIZEDPASSWORD" "$file" | head -n 1 | cut -d ':' -f 1)
-            
-            # Capture the content before "RANDOMIZEDPASSWORD"
-            local config_content=$(sudo sed -n "${line_number}s/^\([^=]*\).*/\1/p" "$file")
-
-            # Update the first occurrence of "RANDOMIZEDPASSWORD" with the new password
-            sudo sed -i "0,/\(RANDOMIZEDPASSWORD\)/s//${random_password}/" "$file"
-            
-            # Display the update message with the captured content and file name
-            isSuccessful "Updated $config_content in $(basename "$file") with a new password."
+            # Check if the file contains the current placeholder
+            if sudo grep -q "$placeholder" "$file"; then
+                # Generate a unique random password if it has not been generated yet
+                if [ -z "${passwords[$i]}" ]; then
+                    passwords[$i]=$(generate_random_password)
+                fi
+                
+                # Update all occurrences of the current placeholder with the new password
+                sudo sed -i "s/${placeholder}/${passwords[$i]}/g" "$file"
+                
+                # Display the update message
+                isSuccessful "Updated ${placeholder} in $(basename "$file") with a new password."
+            fi
         done
+
+        # Next, handle the generic placeholder RANDOMIZEDPASSWORD
+        local placeholder="RANDOMIZEDPASSWORD"
+        
+        # Check if the file contains the placeholder
+        if sudo grep -q "$placeholder" "$file"; then
+            # Generate a unique random password
+            local random_password=$(generate_random_password)
+            
+            # Update all occurrences of the placeholder with the new password
+            sudo sed -i "s/${placeholder}/${random_password}/g" "$file"
+            
+            # Display the update message
+            isSuccessful "Updated ${placeholder} in $(basename "$file") with a new password."
+        fi
     fi
 }
-
