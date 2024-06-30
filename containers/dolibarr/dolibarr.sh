f5de0f04282408f21eb8df981261a03648b8b20e
OpenSourceWebstar <scottwebstar@proton.me>
2024-06-30 21:38:47 +0100

    * Added new app - Dolibarr - ERP System

---
+++ containers/dolibarr/dolibarr.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+# Category : user
+# Description : Dolibarr - CRM System written in PHP (c/u/s/r/i):
+
+installDolibarr()
+{
+    if [[ "$dolibarr" == *[cCtTuUsSrRiI]* ]]; then
+        dockerConfigSetupToContainer silent dolibarr;
+        local app_name=$CFG_DOLIBARR_APP_NAME
+		setupInstallVariables $app_name;
+    fi
+
+    if [[ "$dolibarr" == *[cC]* ]]; then
+        editAppConfig $app_name;
+    fi
+
+    if [[ "$dolibarr" == *[uU]* ]]; then
+        dockerUninstallApp $app_name;
+    fi
+
+    if [[ "$dolibarr" == *[sS]* ]]; then
+        dockerComposeDown $app_name;
+    fi
+
+    if [[ "$dolibarr" == *[rR]* ]]; then
+        dockerComposeRestart $app_name;
+    fi
+
+    if [[ "$dolibarr" == *[iI]* ]]; then
+        echo ""
+        echo "##########################################"
+        echo "###          Install $app_name"
+        echo "##########################################"
+        echo ""
+
+		((menu_number++))
+        echo ""
+        echo "---- $menu_number. Setting up install fuserer and config file for $app_name."
+        echo ""
+
+        dockerConfigSetupToContainer "loud" "$app_name" "install";
+        isSuccessful "Install fuserers and Config files have been setup for $app_name."
+
+        ((menu_number++))
+        echo ""
+        echo "---- $menu_number. Checking & Opening ports if required"
+        echo ""
+
+        portsCheckApp $app_name install;
+        if [[ $disallow_used_port == "true" ]]; then
+            isError "A used port conflict has occured, setup is cancelling..."
+            disallow_used_port=""
+            return
+        else
+            isSuccessful "No used port conflicts found, setup is continuing..."
+        fi
+        if [[ $disallow_open_port == "true" ]]; then
+            isError "An open port conflict has occured, setup is cancelling..."
+            disallow_open_port=""
+            return
+        else
+            isSuccessful "No open port conflicts found, setup is continuing..."
+        fi
+        
+		((menu_number++))
+        echo ""
+        echo "---- $menu_number. Setting up the $app_name docker-compose.yml file."
+        echo ""
+
+        dockerComposeSetupFile $app_name;
+
+		((menu_number++))
+        echo ""
+        echo "---- $menu_number. Updating file permissions before starting."
+        echo ""
+
+		fixPermissionsBeforeStart $app_name;
+
+		((menu_number++))
+        echo ""
+        echo "---- $menu_number. Running the docker-compose.yml to install and start $app_name"
+        echo ""
+
+		dockerComposeUpdateAndStartApp $app_name install;
+
+        ((menu_number++))
+        echo ""
+        echo "---- $menu_number. Running Application specific updates (if required)"
+        echo ""
+
+        appUpdateSpecifics $app_name;
+
+		((menu_number++))
+        echo ""
+        echo "---- $menu_number. Running Headscale setup (if required)"
+        echo ""
+
+		setupHeadscale $app_name;
+
+		((menu_number++))
+		echo ""
+        echo "---- $menu_number. Adding $app_name to the Apps Database table."
+        echo ""
+
+		databaseInstallApp $app_name;
+
+		((menu_number++))
+        echo ""
+        echo "---- $menu_number. You can find $app_name files at $containers_dir$app_name"
+        echo ""
+        echo "    You can now navigate to your $app_name service using any of the options below : "
+        echo ""
+        
+        menuShowFinalMessages $app_name;
+
+		menu_number=0
+        sleep 3s
+        cd
+    fi
+    dolibarr=n
+}
