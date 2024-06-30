7456973fd9fd0ea6b6a900e1033096192249827f
OpenSourceWebstar <scottwebstar@proton.me>
2024-06-30 06:58:59 +0100

    * Fixed config files not being copied.

--- scripts/config/check_configs.sh
+++ scripts/config/check_configs.sh
@@ -6,25 +6,20 @@ checkConfigFilesMissingFiles()
     local missing_files_count=0
 
     for file in "${config_files_all[@]}"; do
-        local file_path="$install_configs_dir$file"
-        if [ -f "$file_path" ]; then
-            if [ ! -f "$configs_dir$file" ]; then
-                copyFile "silent" "$file_path" "$configs_dir$file" "$sudo_user_name"
-                ((missing_files_count++))
-            fi
-            ((file_found_count++))
-        else
-            isNotice "Config File $file does not exist in $install_configs_dir."
+        if [ ! -f "$configs_dir$file" ]; then
+            sudo cp "$install_configs_dir$file" "$configs_dir$file"
+            ((missing_files_count++))
         fi
+        ((file_found_count++))
     done
     
     if [ "$file_found_count" -eq "${#config_files_all[@]}" ]; then
         if [ "$missing_files_count" -gt 0 ]; then
-            isSuccessful "$missing_files_count config files were missing and have been added to the configs folder."
+            echo "${GREEN}SUCCESS:${NC}$missing_files_count config files were missing and have been added to the configs folder."
         else
-            isSuccessful "All config files are successfully set up in the configs folder."
+            echo "${GREEN}SUCCESS:${NC}All config files are successfully set up in the configs folder."
         fi
     else
-        isFatalErrorExit "Not all config files were found in $install_configs_dir."
+        echo "${RED}ERROR:${NC} Not all config files were found in $install_configs_dir."
     fi
 }
