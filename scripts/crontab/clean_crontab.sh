bd4db6dab630480a4a72c9eaf089849ee15ebd43
OpenSourceWebstar <scottwebstar@proton.me>
2024-11-30 19:37:47 +0000

    * Fixed sed data for crontab Clean.

--- scripts/crontab/clean_crontab.sh
+++ scripts/crontab/clean_crontab.sh
@@ -3,10 +3,11 @@
 # Function to clean all crontab
 crontabClean() 
 {
+    data_to_remove = " >> /docker/logs/backup.log 2>&1"
     # Check if the string exists in the crontab
-    if crontab -l | grep -q " >> /docker/logs/backup.log 2>&1"; then
+    if crontab -l | grep -q "$string_to_check"; then
         # If found, remove occurrences of the string from the crontab
-        crontab -l | sed "s/ >> /docker/logs/backup.log 2>&1//g" | crontab -
+        crontab -l | sed "s/$string_to_check//g" | crontab -
         isSuccessful "Removed old data from crontab entries."
     fi
 }
