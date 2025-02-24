77a104dd3abeadcc48dcf84efb031d39270c9a28
OpenSourceWebstar <scottwebstar@proton.me>
2025-02-24 11:02:13 +0000

    * Fixes to remote single restore.

--- scripts/restore/initialize_restore.sh
+++ scripts/restore/initialize_restore.sh
@@ -18,11 +18,11 @@ restoreInitialize()
             migrateEnableConfig;
             backup_save_directory="$backup_single_dir"
             RESTORE_SAVE_DIRECTORY="$restore_single_dir"
-            restoreSingleBackupList;
+            restoreSingleBackupList $backup_save_directory;
         elif [[ "$CFG_REQUIREMENT_MIGRATE" == "true" ]]; then
             backup_save_directory="$backup_single_dir"
             RESTORE_SAVE_DIRECTORY="$restore_single_dir"
-            restoreSingleBackupList;
+            restoreSingleBackupList $backup_save_directory;
         fi
     fi
 }
