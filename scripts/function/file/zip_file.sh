27ac606095a4bb694c4879f383164f727c8a8fca
OpenSourceWebstar <scottwebstar@proton.me>
2025-02-25 06:05:32 +0000

    * Added progress status for backup zip.

--- scripts/function/file/zip_file.sh
+++ scripts/function/file/zip_file.sh
@@ -6,8 +6,11 @@ zipFile()
     local zip_file="$2"
     local zip_directory="$3"
 
-    # Run the SSH command using the existing SSH variables
-    local result=$(sudo zip -r -MM -e -P $passphrase $zip_file $zip_directory)
+    # Calculate the total size of the directory to zip
+    local total_size=$(du -sb "$zip_directory" | awk '{print $1}')
+
+    # Run the SSH command using the existing SSH variables with progress
+    local result=$(tar -cf - "$zip_directory" | pv -s "$total_size" | zip -r -MM -e -P "$passphrase" "$zip_file" -)
     checkSuccess "Zipped up $(basename "$zip_file")"
 
     local result=$(sudo chown $sudo_user_name:$sudo_user_name "$zip_file")
