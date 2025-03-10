ab1e33a5b5ee132cb41fc57480eeae85424fbd20
OpenSourceWebstar <scottwebstar@proton.me>
2025-03-10 20:06:25 +0000

    * More fixes to the Bcrypt password updater.

--- scripts/config/password/update_file.sh
+++ scripts/config/password/update_file.sh
@@ -10,9 +10,9 @@ scanFileForRandomPassword() {
             local placeholder="RANDOMIZEDPASSWORD${i}"
 
             if sudo grep -q "$placeholder" "$file"; then
-                if sudo grep -q "${placeholder}[[:space:]]+--bcrypt" "$file"; then
+                if sudo grep -q "${placeholder}[[:space:]]*--bcrypt" "$file"; then
                     passwords[$i]=$(generateRandomPassword | hashPassword)
-                    sudo sed -i -E "s/${placeholder}[[:space:]]+--bcrypt/${passwords[$i]}/g" "$file"
+                    sudo sed -i -E "s/(${placeholder})[[:space:]]*--bcrypt/${passwords[$i]}/g" "$file"
                     isSuccessful "Updated ${placeholder} with Bcrypt and removed marker in $(basename "$file")."
                 else
                     if [ -z "${passwords[$i]}" ]; then
@@ -27,9 +27,9 @@ scanFileForRandomPassword() {
         # Handle the generic placeholder RANDOMIZEDPASSWORD
         local placeholder="RANDOMIZEDPASSWORD"
         if sudo grep -q "$placeholder" "$file"; then
-            if sudo grep -q "${placeholder}[[:space:]]+--bcrypt" "$file"; then
+            if sudo grep -q "${placeholder}[[:space:]]*--bcrypt" "$file"; then
                 local random_password=$(generateRandomPassword | hashPassword)
-                sudo sed -i -E "s/${placeholder}[[:space:]]+--bcrypt/${random_password}/g" "$file"
+                sudo sed -i -E "s/(${placeholder})[[:space:]]*--bcrypt/${random_password}/g" "$file"
                 isSuccessful "Updated ${placeholder} with Bcrypt and removed marker in $(basename "$file")."
             else
                 local random_password=$(generateRandomPassword)
